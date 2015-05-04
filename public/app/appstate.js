d7e1791b325e6b394acc43a9cf140b350cc1bd26
Matthew Phillips <matthew@matthewphillips.info>
2015-05-04 11:15:15 -0400

    Move appstate into its own file
    This moves the appstate into its own module, so that the autorender
    plugin can use it as the viewmodel for rendering the template.
    
    Also makes a renderNode function that is used in Node, using
    can.route.deparam.

---
+++ public/app/appstate.js
@@ -0,0 +1,5 @@
+import Map from "can/map/map";
+
+const AppState = Map.extend({});
+
+export default AppState;
--- public/app/routes.js
+++ public/app/routes.js
@@ -1,15 +1,6 @@
 import route from 'can/route/route';
 import 'can/route/pushstate/pushstate';
-import Map from 'can/map/map';
-
-import 'less/styles.less!';
-
-const AppState = Map.extend({});
-let state = new AppState();
 
 route(':page', { page: 'home' });
 route(':page/:slug', { slug: null });
 route(':page/:slug/:action', { slug: null, action: null });
-route.map(state);
-
-route.ready();
