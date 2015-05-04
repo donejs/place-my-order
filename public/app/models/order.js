d6e4966ead6463c991640aac79e2daab6fc9d899
David Luecke <daff@neyeon.com>
2015-05-04 12:48:56 -0600

    Real-time connection set up.

--- public/app/models/order.js
+++ public/app/models/order.js
@@ -2,6 +2,7 @@ import 'can/map/define/define';
 import can from 'can';
 import superMap from 'can-connect/super-map';
 import connect  from 'can-connect/can-connect';
+import socket from './socket';
 
 var Order = can.Map.extend({
   define: {
@@ -33,6 +34,10 @@ if(orderConnection.cacheConnection) {
 	orderConnection.cacheConnection.reset();
 }
 
-
+if(socket) {
+  socket.on('api/orders created', order => orderConnection.createInstance(order));
+  socket.on('api/orders updated', order => orderConnection.updateInstance(order));
+  socket.on('api/orders removed', order => orderConnection.destroyInstance(order));
+}
 
 export default Order;
