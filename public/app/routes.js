767286a8567915af8ce6a5382a3e09a2a0de66a7
David Luecke <daff@neyeon.com>
2015-05-07 16:31:10 -0600

    Prevent default pushstate on javascript:// links.

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,6 +1,9 @@
 import route from 'can/route/route';
 import 'can/route/pushstate/pushstate';
+import $ from 'jquery';
 
 route(':page', { page: 'home' });
 route(':page/:slug', { slug: null });
 route(':page/:slug/:action', { slug: null, action: null });
+
+$('body').on('click', 'a[href="javascript://"]', ev => ev.preventDefault());
