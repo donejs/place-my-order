06c53bb30590bfc58df9b96fdbe6772764781ed4
Kyle Gifford <kyle@kylegifford.com>
2015-05-08 17:59:05 -0500

    adds ability for graphs to exist on same scale

--- public/ui/bit_graph/bit_graph.js
+++ public/ui/bit_graph/bit_graph.js
@@ -24,17 +24,22 @@ export var BitSeriesVM = can.Map.extend({
         },
         highestValue: {
             get: function() {
-                var highestNumber = this.attr('serializedData') && d3.max(this.attr('serializedData'));
-                return (highestNumber || highestNumber === 0) ? highestNumber : null;
+                var highestValue = this.attr('serializedData') && d3.max(this.attr('serializedData'));
+                return (highestValue || highestValue === 0) ? highestValue : null;
             }
         },
         line: {
             get: function() {
-                if(this.attr('seriesLength') && this.attr('lowestValue') !== null && this.attr('highestValue') !== null) {
-                    // X scale will fit all values from data[] within pixels 0-w
-                    var x = d3.scale.linear().domain([0, this.attr('seriesLength')]).range([0, this.attr('graphWidth')]);
-                    // Y scale will fit all values from data[] within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-                    var y = d3.scale.linear().domain([this.attr('lowestValue'), this.attr('highestValue')]).range([this.attr('graphHeight'), 0]);
+                if(this.attr('graph') && this.attr('seriesLength') && this.attr('lowestValue') !== null && this.attr('highestValue') !== null) {
+                    if(this.attr('graph').attr('normalize')) {
+                        // X scale will fit all values from data[] within pixels 0-w
+                        var x = d3.scale.linear().domain([0, this.attr('seriesLength')]).range([0, this.attr('graph').attr('width')]);
+                        // Y scale will fit all values from data[] within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+                        var y = d3.scale.linear().domain([this.attr('lowestValue'), this.attr('highestValue')]).range([this.attr('graph').attr('height'), 0]);
+                    } else {
+                        var x = this.attr('graph').attr('xScale');
+                        var y = this.attr('graph').attr('yScale');
+                    }
 
                     // create a line function that can convert this.attr('data')[] into x and y points
                     var line = d3.svg.line()
@@ -67,8 +72,7 @@ export var BitSeriesVM = can.Map.extend({
     },
     color: "steelblue",
     data: null,
-    graphHeight: null,
-    graphWidth: null,
+    graph: null,
     convertListItemsToNumbers: function(listItems) {
         if(listItems) {
                 listItems.each(function(data, index) {
@@ -79,9 +83,21 @@ export var BitSeriesVM = can.Map.extend({
     }
 });
 
+// var graphFixture = can.Map.extend({
+//     width: 100,
+//     height: 100,
+//     normalize: false,
+//     xScale: null,
+//     yScale: null
+// });
+
 // console.log('initial');
 // var vm = new BitSeriesVM({
-//     data: ["23.00", "84.98", "48.99", "138.46"]
+//     data: ["23.00", "84.98", "48.99", "38.46"],
+//     graph: new graphFixture({
+//         xScale: d3.scale.linear().domain([0, 4]).range([0, 100]),
+//         yScale: d3.scale.linear().domain([0, 100]).range([100, 0])
+//     })
 // });
 
 // vm.bind('data', function(e, n, o) {
@@ -113,16 +129,19 @@ export var BitSeriesVM = can.Map.extend({
 // vm.attr('data').push(150);
 // can.batch.stop();
 
+// can.batch.start();
+// vm.attr('graph').attr('xScale', d3.scale.linear().domain([0, 6]).range([0, 100]));
+// vm.attr('graph').attr('yScale', d3.scale.linear().domain([0, 150]).range([100, 0]));
+// can.batch.stop();
+
 can.Component.extend({
     tag: "bit-series",
     viewModel: BitSeriesVM,
     events: {
         inserted: function() {
-            this.scope.attr({
-                graphWidth: this.element.parent().scope().attr('width'),
-                graphHeight: this.element.parent().scope().attr('height')
-            });
-            this.element.parent().scope().addSeries(this.scope);
+            var parentScope = this.element.parent().scope();
+            this.scope.attr('graph', parentScope);
+            parentScope.addSeries(this.scope);
         },
         removed: function() {
             this.element.parent().scope().removeSeries(this.scope);
@@ -133,7 +152,7 @@ can.Component.extend({
 export var BitGraphVM = can.Map.extend({
     define: {
         margins: {
-            value: [0, 0, 0, 80],
+            value: [20, 0, 20, 80],
             type: "*"
         },
         width: {
@@ -165,39 +184,82 @@ export var BitGraphVM = can.Map.extend({
         axisContainerElement: {
             type: "*"
         },
+        normalize: {
+            value: false,
+            type: "htmlbool"
+        },
         seriesSerialized: {
             get: function() {
                 return this.attr('series').serialize();
             }
         },
-        refreshGraph: {
+        longestSeriesLength: {
+            value: 0,
             get: function() {
-                // TODO this method should just capture changes to any element on the graph
-                // TODO append all elements to a document fragment instead and update in the change event
-
-                // bind to the serialized changes
-                this.attr('seriesSerialized');
-
-                this.clearGraphLines(() => {
-                    // Add the line by appending an svg:path element with the data line
-                    var lineContainerElement = this.attr('lineContainerElement');
-                    can.each(this.attr('series'), function(series) {
-                        if(series.attr('line')) {
-                            lineContainerElement.append("svg:path").attr("d", series.attr('line')).style("stroke", series.attr("color"));
-                        }
+                if(this.attr('seriesSerialized') && this.attr('series').attr('length')) {
+                    var longestSeriesLength = 0;
+                    this.attr('series').each(function(s) {
+                        longestSeriesLength = Math.max(longestSeriesLength, s.attr('seriesLength'));
                     });
-                });
+                    return longestSeriesLength;
+                }
+                return 0;
+            }
+        },
+        lowestSeriesValue: {
+            value: 0,
+            get: function() {
+                if(this.attr('seriesSerialized') && this.attr('series').attr('length')) {
+                    var lowestSeriesValue = Infinity;
+                    this.attr('series').each(function(s) {
+                        lowestSeriesValue = Math.min(lowestSeriesValue, s.attr('lowestValue'));
+                    });
+                    return lowestSeriesValue;
+                }
+                return 0;
+            }
+        },
+        highestSeriesValue: {
+            value: 0,
+            get: function() {
+                if(this.attr('seriesSerialized') && this.attr('series').attr('length')) {
+                    var highestSeriesValue = -Infinity;
+                    this.attr('series').each(function(s) {
+                        highestSeriesValue = Math.max(highestSeriesValue, s.attr('highestValue'));
+                    });
+                    return highestSeriesValue;
+                }
+                return 0;
+            }
+        },
+        xScale: {
+            value: null,
+            get: function() {
+                var domainEndValue = (this.attr('normalize')) ? 5 : this.attr('longestSeriesLength');
+
+                // X scale will fit all values from data[] within pixels 0-w
+                return d3.scale.linear().domain([0, domainEndValue]).range([0, this.attr('width')]);
+            }  
+        },
+        yScale: {
+            value: null,
+            get: function() {
+                var normalize = this.attr('normalize'),
+                    domainBeginValue = normalize ? 0 : this.attr('lowestSeriesValue'),
+                    domainEndValue = normalize ? 5 : this.attr('highestSeriesValue');
+
+                // Y scale will fit all values from data[] within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+                return d3.scale.linear().domain([domainBeginValue, domainEndValue]).range([this.attr('height'), 0]);
             }
         }
     },
     series: [],
-    clearGraphLines: function(cb) {
+    clearContainer: function(container, cb) {
         // no clean way to remove all elements and receive a callback, so we have to make our own
         // http://stackoverflow.com/questions/23118779/d3-callback-function-after-remove
-        var counter = 0,
-            lineContainerElement = this.attr('lineContainerElement');
-        if(lineContainerElement) {
-            lineContainerElement.selectAll('*').call(function(selection) {
+        var counter = 0;
+        if(container) {
+            container.selectAll('*').call(function(selection) {
                 counter = selection.size();
                 // if counter is zero, the each won't ever execute, so fire the callback here also
                 if(counter === 0) {
@@ -211,16 +273,44 @@ export var BitGraphVM = can.Map.extend({
             }).remove();
         }
     },
+    clearGraphLines: function(cb) {
+        this.clearContainer(this.attr('lineContainerElement'), cb);
+    },
+    refreshGraph: function() {
+        // TODO this method should just capture changes to any element on the graph and move into a define
+        // TODO append all elements to a document fragment instead and update in the change event
+
+        // bind to the serialized changes
+        this.attr('seriesSerialized');
+
+        this.clearGraphLines(() => {
+            // Add the line by appending an svg:path element with the data line
+            var lineContainerElement = this.attr('lineContainerElement');
+            can.each(this.attr('series'), function(series) {
+                if(series.attr('line')) {
+                    lineContainerElement.append("svg:path").attr("d", series.attr('line')).style("stroke", series.attr("color"));
+                }
+            });
+        });
+    },
+    clearAxes: function(cb) {
+        this.clearContainer(this.attr('axisContainerElement'), cb);
+    },
     refreshAxes: function() {
-        var axisContainerElement = this.attr('axisContainerElement');
-        if(axisContainerElement) {
-            // X scale will fit all values from data[] within pixels 0-w
-            var x = d3.scale.linear().domain([0, 5]).range([0, this.attr('width')]);
-            // Y scale will fit all values from data[] within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-            var y = d3.scale.linear().domain([0, 5]).range([this.attr('height'), 0]);
+        this.clearAxes(() => {
+            var axisContainerElement = this.attr('axisContainerElement');
 
             // create xAxis
-            var xAxis = d3.svg.axis().scale(x).tickSize(-this.attr('height')).tickSubdivide(true).tickFormat("");
+            var xAxis = d3.svg.axis().scale(this.attr('xScale')).tickSize(-this.attr('height')).tickSubdivide(true).tickFormat("");
+
+            // create left yAxis
+            var yAxisLeft = d3.svg.axis().scale(this.attr('yScale')).ticks(4).orient("left");
+
+            // if we are normalizing, remove the values
+            if(this.attr('normalize')) {
+                xAxis.tickFormat("");
+            }
+
             // Add the x-axis.
             axisContainerElement.append("svg:g")
                   .attr("class", "x axis")
@@ -228,15 +318,13 @@ export var BitGraphVM = can.Map.extend({
                   .attr("transform", "translate(0," + this.attr('height') + ")")
                   .call(xAxis);
 
-            // create left yAxis
-            var yAxisLeft = d3.svg.axis().scale(y).ticks(4).tickFormat("").orient("left");
             // Add the y-axis to the left
             axisContainerElement.append("svg:g")
                   .attr("class", "y axis")
                   .attr("height", this.attr('height'))
                   .attr("transform", "translate(-25,0)")
                   .call(yAxisLeft);
-        }
+        });
     },
     renderBaseGraph: function(graphBaseElement) {
         var margins = this.attr('margins');
@@ -253,6 +341,7 @@ export var BitGraphVM = can.Map.extend({
 
         // TODO remove this eventually and make axes dependent on line data?
         this.refreshAxes();
+        this.refreshGraph();
     },
     addSeries: function(series) {
         can.batch.start();
@@ -267,28 +356,75 @@ export var BitGraphVM = can.Map.extend({
     }
 });
 
-// var bs1 = new BitSeriesVM({
-//     data: [1,2,3]
-// });
-// var bs2 = new BitSeriesVM({
-//     data: [100,200,300,400]
+// console.log('initial');
+// var vm = new BitGraphVM({
+//     normalize: false,
+//     width: 100,
+//     height: 100
 // });
 
-// console.log('initial');
-// var vm = new BitGraphVM({});
+// var bs1 = new BitSeriesVM({
+//     data: ["23.00", "84.98", "48.99", "38.46"],
+//     graph: vm
+// });
+// // var bs2 = new BitSeriesVM({
+// //     data: [100,200,300,400],
+// //     graph: vm
+// // });
+// // var bs3 = new BitSeriesVM({
+// //     data: [1,4,6],
+// //     graph: vm
+// // });
+
+// vm.bind('series', function(e, n, o) {
+//     console.log('series changed from ', o, ' to ', n);
+// });
+// vm.bind('seriesSerialized', function(ev, n, o) {
+//     console.log('seriesSerialized changed from ', o, ' to ', n);
+// });
+// vm.bind('longestSeriesLength', function(ev, n, o) {
+//     console.log('longestSeriesLength changed from ', o, ' to ', n);
+// });
+// vm.bind('lowestSeriesValue', function(ev, n, o) {
+//     console.log('lowestSeriesValue changed from ', o, ' to ', n);
+// });
+// vm.bind('highestSeriesValue', function(ev, n, o) {
+//     console.log('highestSeriesValue changed from ', o, ' to ', n);
+// });
+// vm.bind('xScale', function(ev, n, o) {
+//     console.log('xScale changed');
+// });
+// vm.bind('yScale', function(ev, n, o) {
+//     console.log('yScale changed');
+// });
+
+// bs1.bind('line', function(ev, n, o) {
+//     console.log('bs1 line changed from ', o, ' to ', n);
+// });
+
+// // bs3.bind('line', function(ev, n, o) {
+// //     console.log('bs3 line changed from ', o, ' to ', n);
+// // });
 
 // vm.addSeries(bs1);
-// vm.addSeries(bs2);
+// // vm.addSeries(bs2);
+// // vm.addSeries(bs3);
 
-// can.batch.start();
-// console.log('pushing 0');
-// bs1.attr('data').push(0);
-// can.batch.stop();
+// // can.batch.start();
+// // console.log('pushing 0');
+// // bs1.attr('data').push(0);
+// // can.batch.stop();
 
-// can.batch.start();
-// console.log('pushing 500');
-// bs2.attr('data').push(500);
-// can.batch.stop();
+// // // can.batch.start();
+// // // console.log('pushing 500');
+// // // bs2.attr('data').push(500);
+// // // can.batch.stop();
+
+// // can.batch.start();
+// // console.log('pushing 7 and 8');
+// // bs3.attr('data').push(7);
+// // bs3.attr('data').push(8);
+// // can.batch.stop();
 
 can.Component.extend({
     tag: "bit-graph",
@@ -300,9 +436,14 @@ can.Component.extend({
             var graphBaseElement = d3.select(document.getElementById('graph'))
             this.scope.renderBaseGraph(graphBaseElement);
         },
-        "{refreshGraph} change": function(a, b, c) {
-            // check into grouped elements
-            console.log('TODO rebuild the graph here w ', c);
+        "{scope} xScale": function() {
+            this.scope.refreshAxes();
+        },
+        "{scope} yScale": function() {
+            this.scope.refreshAxes();
+        },
+        "{scope} seriesSerialized": function() {
+            this.scope.refreshGraph();
         }
     }
 });
