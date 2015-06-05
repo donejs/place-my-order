60d1de8366ddcea50571c11255295b116a3f3248
Manuel Mujica <evilsaurer@gmail.com>
2015-06-05 10:24:11 -0300

    Disable "Place my order" button if no items selected.
    Closes #51.

--- pmo/order/order_test.js
+++ pmo/order/order_test.js
@@ -11,6 +11,24 @@ test('Default order is initialized', () => {
   ok(vm.attr('order') instanceof Order);
 });
 
+test('canPlaceOrder indicates whether the order has items', () => {
+  let vm = new ViewModel();
+  let items = vm.attr('order.items');
+
+  let item = {
+    name: 'pabellon criollo',
+    price: 35.90
+  };
+
+  ok(!items.attr('length'), 'order has no items');
+  ok(!vm.attr('canPlaceOrder'), 'user can not place order without items');
+
+  items.push(item);
+
+  equal(items.attr('length'), 1, 'order has 1 item');
+  ok(vm.attr('canPlaceOrder'), 'user can place the order');
+});
+
 asyncTest('Setting slug gets a restaurant and it is added to order', () => {
   let vm = new ViewModel({ slug: 'spago', '@root': new AppState() });
   let deferred = vm.attr('restaurant');
