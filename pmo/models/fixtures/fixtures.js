3eb35a1f0170d45a834644ff41fb5281b071212a
David Luecke <daff@neyeon.com>
2015-06-11 10:25:25 -0600

    Fixing place-my-order tests

--- pmo/models/fixtures/fixtures.js
+++ pmo/models/fixtures/fixtures.js
@@ -1,4 +1,4 @@
 import fixture from 'can/util/fixture/';
 
-fixture('GET /api/restaurants', '/pmo/models/fixtures/restaurants.json');
-fixture('GET /api/restaurants/{_id}', '/pmo/models/fixtures/spago.json');
+fixture('GET /api/restaurants', 'pmo/models/fixtures/restaurants.json');
+fixture('GET /api/restaurants/{_id}', 'pmo/models/fixtures/spago.json');
