18ef567df06f3e80d975331a59b997b08c506a18
Matthew Phillips <matthew@matthewphillips.info>
2018-11-08 14:06:37 -0500

    Remove unused build parts

--- build.js
+++ build.js
@@ -1,6 +1,3 @@
-var buildCordova = process.argv.indexOf("cordova") > 0;
-var buildElectron = process.argv.indexOf("electron") > 0;
-
 const stealTools = require("steal-tools");
 
 let buildPromise = stealTools.build({
@@ -13,45 +10,3 @@ let buildPromise = stealTools.build({
     glob: "node_modules/place-my-order-assets/images/**/*"
   }
 });
-// options added by `donejs add cordova` - START
-var cordovaOptions = {
-  buildDir: "./build/cordova",
-  id: "com.donejs.placemyorder",
-  name: "place my order",
-  platforms: ["ios"],
-  plugins: ["cordova-plugin-transport-security"],
-  index: __dirname + "/production.html",
-  glob: [
-    "node_modules/place-my-order-assets/images/**/*"
-  ]
-};
-
-var stealCordova = require("steal-cordova")(cordovaOptions);
-
-if(buildCordova) {
-  buildPromise.then(stealCordova.build).then(stealCordova.ios.emulate);
-}
-// options added by `donejs add cordova` - END
-// options added by `donejs add electron` - START
-var electronOptions = {
-  main: "electron-main.js",
-  buildDir: "./build",
-  platforms: ["darwin"],
-  archs: ["x64"],
-  glob: [
-    "package.json",
-    "production.html",
-    "node_modules/steal/steal.production.js"
-  ]
-};
-
-var stealElectron = require("steal-electron");
-
-if(buildElectron) {
-  buildPromise = buildPromise.then(function(buildResult){
-    return stealElectron(electronOptions, buildResult);
-  }).catch(function(err) {
-    console.log(err);
-  });
-}
-// options added by `donejs add electron` - END
