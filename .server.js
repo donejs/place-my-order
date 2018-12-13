3e88af1df5ae337ca1bcc4aebdc2caed8cd2d5c9
Matthew Phillips <matthew@matthewphillips.info>
2018-12-13 14:02:00 -0500

    Allowed the local server to configure
    The server and inject middleware.

--- .server.js
+++ .server.js
@@ -4,7 +4,18 @@ var server = require('done-serve');
 var port = process.env.PORT || 3030;
 
 var app = server(port, {
-  path: __dirname
+  path: __dirname,
+  configure: function(layers) {
+    var apiExp = /^\/api/;
+    layers.push(function(req, res, next) {
+      if(apiExp.test(req.url)) {
+        req.url = req.url.split(apiExp)[1];
+        api(req, res, next);
+        return;
+      }
+      next();
+    });
+  }
 });
 
 api.setup(app);
