4617e41fb01655dcfbbbe76717255b19bdd114d3
Connor Graham <connor@bitovi.com>
2020-11-05 15:42:41 -0500

    Change DOCKER_ORG to bitovi

--- docker-publish.sh
+++ docker-publish.sh
@@ -7,7 +7,7 @@ if [ $TRAVIS_BRANCH == 'introduce-docker' ] && [ "$TRAVIS_PULL_REQUEST" == 'fals
   docker login -u $DOCKER_USER -p $DOCKER_PASS
 
   # These can be paramaterized or set up via the pipeline runner
-  DOCKER_ORG="mickmcgrath13"
+  DOCKER_ORG="bitovi"
   DOCKER_REPO="place-my-order"
   DOCKER_PATH="$DOCKER_ORG/$DOCKER_REPO"
   DOCKER_TAG="latest"
