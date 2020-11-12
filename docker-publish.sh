962bc9549b7d06c171e7ab155a4f482d0ed99bfa
Connor Graham <connor@bitovi.com>
2020-11-12 14:00:06 -0500

    Update Docker publish information

--- docker-publish.sh
+++ docker-publish.sh
@@ -7,10 +7,11 @@ if [ $TRAVIS_BRANCH == 'introduce-docker' ] && [ "$TRAVIS_PULL_REQUEST" == 'fals
   docker login -u $DOCKER_USER -p $DOCKER_PASS
 
   # These can be paramaterized or set up via the pipeline runner
-  DOCKER_ORG="bitovi"
+  DOCKER_HOST="ghcr.io"
+  DOCKER_ORG="donejs"
   DOCKER_REPO="place-my-order"
-  DOCKER_PATH="$DOCKER_ORG/$DOCKER_REPO"
-  DOCKER_TAG="latest"
+  DOCKER_PATH="$DOCKER_HOST/$DOCKER_ORG/$DOCKER_REPO"
+  DOCKER_TAG="$TRAVIS_COMMIT"
 
   echo "Building the container with the: $DOCKER_PATH:$DOCKER_TAG"
 
